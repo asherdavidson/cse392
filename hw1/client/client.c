d3f3fd689d757ab17fe0523c275b5a0adc460f00
azdavidson <azdavidson@cs.stonybrook.edu>
2018-02-09 12:43:12 -0500

    cleaned up code

--- hw1/client/client.c
+++ hw1/client/client.c
@@ -15,14 +15,15 @@ void exit_error(char *msg) {
 
 
 int init_socket(const char *address, const char *port) {
-    // get the address
     struct addrinfo hints;
     struct addrinfo *result;
 
+    // zero out and write our hints
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_INET; // TODO: ipv6 support?
     hints.ai_socktype = SOCK_STREAM;
 
+    // get the linked-list of results
     if (getaddrinfo(address, port, &hints, &result) != 0) {
         exit_error("error in getaddrinfo");
     }
@@ -30,20 +31,26 @@ int init_socket(const char *address, const char *port) {
     struct addrinfo *next;
     int fd;
 
+    // find the first valid result
     for (next = result; next != NULL; next = next->ai_next) {
+        // try to open a socket
         fd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 
-        if (fd == -1) continue;
+        if (fd == -1) continue; // bad socket!
 
+        // connection okay!
         if (connect(fd, result->ai_addr, result->ai_addrlen) != -1) break;
 
+        // socket didn't connect, so close it to be safe
         close(fd);
     }
 
+    // didn't find a socket
     if (next == NULL) {
         exit_error("No valid address found");
     }
 
+    // cleanup
     freeaddrinfo(result);
 
     return fd;
@@ -57,23 +64,25 @@ int main(int argc, char const *argv[])
         exit_error("invalid args");
     }
 
+    // args
     const char *address = argv[2];
     const char *port = argv[3];
     
+    // get socket connection
     int fd = init_socket(address, port);
 
+    // historic first words
     write(fd, "hello\n", 7);
 
+    // they respond
     char buf[BUF_SIZE];
     memset(&buf, 0, sizeof(buf));
     read(fd, &buf, sizeof(buf));
 
     printf("%s\n", buf);
 
+    // good bye
     close(fd);
 
-    
-
-
-    return 0;
+    return EXIT_SUCCESS;
 }
