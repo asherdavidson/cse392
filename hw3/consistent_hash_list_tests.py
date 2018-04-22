af5828e84a1cfcf589ed2107e80b1a0fef2e8326
flou <freeman.lou@stonybrook.edu>
2018-04-21 21:04:41 -0400

    added more unit tests

--- hw3/consistent_hash_list_tests.py
+++ hw3/consistent_hash_list_tests.py
@@ -6,17 +6,39 @@ class TestConsistentHashList(unittest.TestCase):
         self.list = ConsistentHashList(1000000)
 
     def test_add_client(self):
-        self.list.add_client('192.168.1.1')
-        self.list.add_client('192.168.1.35')
-        self.list.add_client('192.168.1.61')
-        self.list.add_client('192.168.1.11')
-        self.list.add_client('192.168.1.6')
-        self.list.add_client('192.168.1.21')
-        self.list.add_client('192.168.1.4')
-        self.list.add_client('192.168.1.71')
+        self.list.add_client('192.168.1.1')     # 280952 
+        self.list.add_client('192.168.1.35')    # 957443
+        self.list.add_client('192.168.1.61')    # 925877
+        self.list.add_client('192.168.1.11')    # 187424
+        self.list.add_client('192.168.1.6')     # 809407
+        self.list.add_client('192.168.1.21')    # 788830
+        self.list.add_client('192.168.1.4')     # 267522
+        self.list.add_client('192.168.1.71')    #  30457
         print(self.list)
 
         self.assertEqual(len(self.list), 8, 'incorrect length')
+
+    def test_get_client(self):
+        self.list.add_client('192.168.1.21')    # 788830
+        self.list.add_client('192.168.1.4')     # 267522
+        self.list.add_client('192.168.1.71')    #  30457
+
+        self.assertEqual(self.list.get_client(50252), '192.168.1.4', 'wrong client for hash')
+
+    def test_get_client_end(self):
+        # Test wrap around
+        self.list.add_client('192.168.1.21')    # 788830
+        self.list.add_client('192.168.1.4')     # 267522
+        self.list.add_client('192.168.1.71')    #  30457
+
+        self.assertEqual(self.list.get_client(800000), '192.168.1.71', 'wrong client for hash')
+
+    def test_get_client_start(self):
+        self.list.add_client('192.168.1.21')    # 788830
+        self.list.add_client('192.168.1.4')     # 267522
+        self.list.add_client('192.168.1.71')    #  30457
+
+        self.assertEqual(self.list.get_client(20000), '192.168.1.71', 'wrong client for hash')
 
 
 if __name__ == "__main__":
